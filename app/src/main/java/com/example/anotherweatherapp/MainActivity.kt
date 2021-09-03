2ae6c56fc78ecd2c53dc06d497fdfe4a0527a562
Guillermo <guilogar42@gmail.com>
2021-09-03 20:12:07 +0200

    First layout, floating button and toolbar

--- app/src/main/java/com/example/anotherweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/anotherweatherapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.anotherweatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var citySelected: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Temporal array of cities
+        var cities = arrayListOf<String>("Valdemoro", "Ibiza", "Titulcia", "Madrid")
+
+
+        //Show a message if there is not a city selected
+        var noCityMessage: TextView = findViewById<EditText>(R.id.noCityText)
+        if (!citySelected){
+            noCityMessage.setText(R.string.no_city)
+        } else {
+            noCityMessage.visibility = View.INVISIBLE
+        }
+
+
+
+
     }
+
 }
