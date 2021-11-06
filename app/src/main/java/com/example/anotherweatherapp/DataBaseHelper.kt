43a7fb867de99e1abefcea4f707c319f62ddcfab
Guillermo <guilogar42@gmail.com>
2021-11-06 20:55:44 +0100

    Database and some layouts

---
+++ app/src/main/java/com/example/anotherweatherapp/DataBaseHelper.kt
@@ -0,0 +1,39 @@
+package com.example.anotherweatherapp
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.provider.BaseColumns
+
+class DataBaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    object bbdd {
+        // Table contents are grouped together in an anonymous object.
+        object baseObject : BaseColumns {
+            const val TABLE_NAME = "Ciudades"
+            const val CITY_NAME = "Nombre"
+
+        }
+    }
+
+    private val SQL_CREATE_ENTRIES =
+        "CREATE TABLE ${bbdd.baseObject.TABLE_NAME} (" +
+                "id INTEGER PRIMARY KEY," +
+                "${bbdd.baseObject.CITY_NAME} TEXT)"
+
+    private val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${bbdd.baseObject.TABLE_NAME}"
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(SQL_CREATE_ENTRIES)
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+
+    }
+
+    companion object {
+        // Common object for all the instances
+        const val DATABASE_VERSION = 1
+        const val DATABASE_NAME = "DataBase"
+    }
+}
--- app/src/main/java/com/example/anotherweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/anotherweatherapp/MainActivity.kt
@@ -1,24 +1,51 @@
 package com.example.anotherweatherapp
 
+import android.content.ContentValues
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.BaseColumns
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.anotherweatherapp.DataBaseHelper.bbdd.baseObject.CITY_NAME
+import com.example.anotherweatherapp.DataBaseHelper.bbdd.baseObject.TABLE_NAME
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private var dbHelper: DataBaseHelper = DataBaseHelper(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //Temporal array of cities
-        var cities = arrayListOf<String>("Valdemoro", "Ibiza", "Titulcia", "Madrid",)
+        var cities: ArrayList<String> = getCities()
+
+
+        //search city button
+
+        setContentView(R.layout.add_city)
+        var searchButton: Button = findViewById<Button>(R.id.button_search)
+        searchButton.setOnClickListener {
+            var cityName: TextView = findViewById<TextView>(R.id.enter_city_edit_text)
+            addCity(cityName.text.toString())
+
+        }
+
+        //floating button action when clicked
+        setContentView(R.layout.floating_button_layout)
+        var addCityButton: FloatingActionButton = findViewById<FloatingActionButton>(R.id.fab)
+        addCityButton.setOnClickListener {
+            setContentView(R.layout.add_city)
+        }
 
         setContentView(R.layout.no_cities)
+
         //Show a message if there is not a city selected
         var noCityMessage: TextView = findViewById<TextView>(R.id.no_city_text)
-        if (cities.size == 0){
+        if (cities.size == 0) {
             noCityMessage.setText(R.string.no_city)
         } else {
 
@@ -32,8 +59,49 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+    }
 
+    fun getCities(): ArrayList<String> {
+        var cities = arrayListOf<String>()
+        var dbRead = dbHelper.readableDatabase
 
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        val projection = arrayOf(CITY_NAME)
+        val cursor = dbRead.query(
+            TABLE_NAME,         // The table to query
+            projection,         // The array of columns to return (pass null to get all)
+            null,       // The columns for the WHERE clause
+            null,    // The values for the WHERE clause
+            null,       // don't group the rows
+            null,        // don't filter by row groups
+            null        // The sort order
+        )
+
+        with(cursor) {
+            while (moveToNext()) {
+                val itemId = getString(getColumnIndexOrThrow(BaseColumns._ID))
+                cities.add(itemId)
+            }
+        }
+        return cities
+    }
+
+    fun addCity(name: String): Boolean {
+        var done = false
+
+        // Gets the data repository in write mode
+        val db = dbHelper.writableDatabase
+
+        // Create a new map of values, where column names are the keys
+        val values = ContentValues().apply {
+            put(CITY_NAME, name)
+        }
+
+        // Insert the new row, returning the primary key value of the new row
+        val newRowId = db?.insert(TABLE_NAME, null, values)
+
+        return done
     }
 
 }
